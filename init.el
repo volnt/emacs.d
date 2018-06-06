3cc4b5b16dbfc317ea05257ce973398b929be178
Florent Espanet <volent@green.home>
2018-06-06 22:39:08 +0200

    Initial commit
    * Use [use-package](https://github.com/jwiegley/use-package) to manage
    packages.
    * Include `magit` as a first package

---
+++ init.el
@@ -0,0 +1,11 @@
+(package-initialize)
+(eval-when-compile
+  (add-to-list 'load-path "~/.emacs.d/site-lisp/use-package/")
+  (require 'use-package))
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+
+(use-package magit
+  :ensure t
+  :bind ("C-c s" . magit-status))
