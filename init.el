95f2245f93e22301a56c1244ee473577cc7a88dc
Florent Espanet <volent@green.home>
2018-06-06 22:53:38 +0200

    Move package-archives before use-package require

--- init.el
+++ init.el
@@ -2,14 +2,17 @@
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
 
+;; Load use-package
 (package-initialize)
-(eval-when-compile
-  (add-to-list 'load-path "~/.emacs.d/site-lisp/use-package/")
-  (require 'use-package))
 
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 
+(eval-when-compile
+  (add-to-list 'load-path "~/.emacs.d/site-lisp/use-package/")
+  (require 'use-package))
+
+;; Install and setup packages
 (use-package magit
   :ensure t
   :bind ("C-c s" . magit-status))
