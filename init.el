be2fbea123da0a93e07d9c442ef37959cbfe76dd
Florent Espanet <volent@green.home>
2018-06-06 23:40:11 +0200

    Add projectile package

--- init.el
+++ init.el
@@ -43,3 +43,8 @@
   :ensure t
   :bind ("C-c i" . py-isort-before-save)
   :custom (py-isort-options '("-w 120")))
+
+(use-package projectile
+  :ensure t
+  :bind ("C-c f" . projectile-find-file)
+  :config (projectile-global-mode))
